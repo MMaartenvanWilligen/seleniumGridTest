3e34aa27cbb207a3f8bec5fc4954854c58353805
MMaartenvanWilligen <maarten@connectholland.nl>
2016-11-07 11:06:33 +0100

    stable test local

--- test.js
+++ test.js
@@ -2,20 +2,6 @@
  * Created by maarten on 03-11-16.
  */
 
-
- /*var assert = require("assert");
-
- describe("my webdriverio tests", function(){
- it("Google test",function() {
- return browser.url("https://www.google.com").then(function () {
- return browser.getTitle().then(function () {
- console.log("Title was: " + title);
- return assert(title === "Google");
- });
- });
- });
- });*/
-
 var assert = require('assert');
 describe('my webdriverio tests', function(){
     it('Github test', function() {
