02c10e70a0a2947d3ae810db2a9555b354e69b97
Maarten van Willigen <maarten@maarten.choffice.nl>
2016-11-09 17:04:39 +0100

    start.sh upgrade. grid/console works

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 echo "kill firefox and screens"
 pkill firefox
 killall Xvfb
+kill `sudo lsof -t -i:4444`
 
 echo "install java"
 
@@ -13,16 +14,29 @@ echo "install xvfb, firefox and imagemagick "
 
 apt-get install xvfb firefox imagemagick
 
-echo "start display:1"
+echo "start display:2"
 
-Xvfb :1 -screen 0 1280x960x24 &
-DISPLAY=:1 firefox http://www.google.com &
+Xvfb :2 -screen 0 1280x960x24 &
+
+echo "start selenium hub"
+
+java -jar selenium-server-standalone-2.53.1.jar -role hub &
 sleep 10
 
-echo "make screenshot with imagemagick. display 1. image file in sources"
+echo "start selenium node"
+java -jar selenium-server-standalone-2.53.1.jar -role node -hub http://172.17.0.1:4444/grid/register/ -browser browserName=firefox,maxInstances=5
+sleep 10
 
-DISPLAY=:1 import -window root -crop 1264x948+0+0 -resize 1264x948 -quality 90 /var/www/sources/screenshot.jpg
+echo "start firefox go to grid console"
 
-echo "kill firefox"
+DISPLAY=:2 firefox http://localhost:4444/grid/console &
+sleep 10
 
+echo "make screenshot with imagemagick. display 2. image file in sources"
+
+DISPLAY=:2 import -window root -crop 1264x948+0+0 -resize 1264x948 -quality 90 /var/www/sources/seleniumGridTest/screenshot.jpg &
+
+echo "kill firefox and screens"
 pkill firefox
+killall Xvfb
+kill `sudo lsof -t -i:4444`
