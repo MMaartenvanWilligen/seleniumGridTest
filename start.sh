a4c8089843e36c855efc0ffcead58024aee69cc1
MMaartenvanWilligen <maarten@connectholland.nl>
2016-11-23 17:00:29 +0100

    init chrome node in start.sh for parallel testing

--- start.sh
+++ start.sh
@@ -11,6 +11,10 @@ echo "Starting up Selenium Grid node firefox "
 java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://localhost:4444/grid/register -browser browserName=firefox &
 sleep 10
 
+echo "Starting up Selenium Grid node chrome"
+java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5556 -hub http://localhost:4444/grid/register -browser browserName=chrome &
+sleep 10
+
 echo "start wdio.conf.js"
 ./node_modules/.bin/wdio wdio.conf.js
 sleep 10
