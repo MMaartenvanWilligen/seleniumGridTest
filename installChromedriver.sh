299f48e2d61148f5e01a141fe820377097a003bc
MMaartenvanWilligen <maarten@connectholland.nl>
2016-11-23 16:45:08 +0100

    first check if chromedriver already exists

--- installChromedriver.sh
+++ installChromedriver.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -f "/usr/bin/chromedriver" ]; then # Will enter here if $DIRECTORY exists, even if it contains spaces
+echo "chromedriver already exists"
+
+else
+echo "install chromedriver file"
+
 curl -O https://chromedriver.storage.googleapis.com/2.25/chromedriver_mac64.zip
 unzip chromedriver_mac64.zip
 rm -f /usr/local/bin/chromedriver
 mv chromedriver /usr/local/bin/
 rm -f chromedriver_mac64.zip
 
+fi
