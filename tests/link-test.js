8b042a749ad4fa102c33102e93ef5e5c4f47fd8f
Maarten van Willigen <mmaartenvanwilligen@gmail.com>
2016-11-24 14:40:04 +0100

    Update link-test.js

--- tests/link-test.js
+++ tests/link-test.js
@@ -1,14 +1,14 @@
 /**
  * Created by maarten on 11-11-16.
  */
-
+//
 var assert = require('assert');
 var chai = require('chai')
     , expect = chai.expect
     , should = chai.should();
 
 var HomepageMindBlue = require("./page-objects/homepage-mind-blue");
-
+//
 
 describe("test anchor tag", function () {
 
