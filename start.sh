dbf30e48e67e6ffd3f3bb1c78176fc83bc2a504f
Maarten van Willigen <maarten@maarten.choffice.nl>
2016-11-28 15:37:55 +0100

    update echo in start.sh

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ echo "start selenium hub"
 java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role hub &
 sleep 10
 
-echo "Starting up Selenium Grid node "
+echo "Starting up Selenium Grid node firefox"
 DISPLAY=:88 xvfb-run java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://localhost:4444/grid/register -browser browserName=firefox,platform=LINUX,maxInstances=5 &
 sleep 10
 
