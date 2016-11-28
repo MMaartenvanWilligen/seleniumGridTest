559d7833a716c8238ca85ee340713d997bd501cc
Maarten van Willigen <maarten@maarten.choffice.nl>
2016-11-28 15:22:21 +0100

    improve and add comment lines to wdio.conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,14 +4,13 @@ exports.config = {
 
     /**
      * @desc capabilities
-     *       declaration in which browsers to test
-     *       testing in browser: chrome, firefox and internet explorer
-     *       saucelabs platform and browsers you can use: https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+     * declaration in which browsers to test
+     * testing in browser: firefox
      */
 
     capabilities: [
         {
-            maxInstances: 1,
+            maxInstances: 1,                            // maximal test files to run
             browserName: "firefox"
         }],
 
@@ -19,27 +18,27 @@ exports.config = {
      * @desc test configurations
      *
      * maxInstances: as you have 3 different capabilities (Chrome, Firefox and Internet Explorer) and you have
-     * set maxInstances to 1, wdio will spawn 3 processes in suacelabs. Therefor if you have 10 spec
+     * set maxInstances to 1, wdio will spawn 3 processes in the selenium grid. Therefor if you have 10 test
      * files and you set maxInstances to 10, all test files will get tested at the same time
-     * and 30 processes will get spawned in saucelabs.
+     * and 30 processes will get spawned in the selenium grid.
      *
      */
 
-    maxInstances: 5,                                     //max test files to run
-    host: '172.17.0.1',                                  //declare selenium grid host
-    port: 4444,                                          //port host
+    maxInstances: 5,                                     // maximal test files to run in total
+    host: '172.17.0.1',                                  // declare selenium grid host
+    port: 4444,                                          // port host
     logLevel: 'silent',
-    marionette: true,
-    sync: false,
+    marionette: true,                                    // Geckodriver is marionette driver
+    sync: false,                                         // synchronous to false
     coloredLogs: true,
     waitforTimeout: 10000,
     reporters: ['dot'],
     reporterOptions: {
         outputDir: './'
     },
-    framework: 'mocha',                                  //run tests with framework mocha
+    framework: 'mocha',                                  // run tests with framework mocha
     mochaOpts: {
-        ui: 'bdd',
+        ui: 'bdd',                                       // bdd: Behaviour drive development
         timeout: 99999999
     },
 
@@ -48,7 +47,7 @@ exports.config = {
      */
 
     specs: [
-        './tests/*-test.js'                          //run all files in map 'test' with ' -title-test.js' in name
+        './tests/*-test.js'                          //run all files in map 'test' with '-test.js' in name
     ]
 
 };
