3c4979da97cec36460ed7a0be6a648e5e259566a
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-02 10:36:17 +0100

    startNodeIe

--- startNodeIE.sh
+++ startNodeIE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://10.123.123.87:4444/grid/register/ -browser browserName=IE,platform=Windows,maxInstances=5 &
+java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://10.123.123.87:4444/grid/register/  -browser browserName=MicrosoftEdge,platform=Windows,maxInstances=5 &
