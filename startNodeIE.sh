26b102aafcf90a0c42caa0dae742c96e409f4603
IEUser <IEUser>
2016-12-02 05:33:24 -0800

    add driver and conf ie

--- startNodeIE.sh
+++ startNodeIE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://10.123.123.87:4444/grid/register/  -browser browserName=MicrosoftEdge,platform=Windows,maxInstances=5 &
+java -Dwebdriver.edge.driver=selenium-grid/MicrosoftWebDriver.exe -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -port 5555 -hub http://10.123.123.87:4444/grid/register/ -browser browserName=MicrosoftEdge,platform=Windows,maxInstances=5 &
