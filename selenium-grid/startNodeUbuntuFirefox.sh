1979cfc4025253049fbccc9041afb4c2f28a4d28
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-05 15:20:45 +0100

    set path to geckodriver

--- selenium-grid/startNodeUbuntuFirefox.sh
+++ selenium-grid/startNodeUbuntuFirefox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dwebdriver.firefox.bin=drivers/geckodriver -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeFirefoxConfiguration.json
+java -Dwebdriver.gecko.driver=drivers/geckodriver -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeFirefoxConfiguration.json
