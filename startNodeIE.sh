a15fe8392ba346b1359985c3e04b6344d05889f8
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-05 11:36:46 +0100

    start node with configuration file and set path edge driver with -Ddriver.edge.driver

--- startNodeIE.sh
+++ startNodeIE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -nodeConfig conf/seleniumNodeEdgeConfiguration.json
+java -Dwebdriver.edge.driver=selenium-grid/MicrosoftWebDriver.exe -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -nodeConfig conf/seleniumNodeEdgeConfiguration.json
