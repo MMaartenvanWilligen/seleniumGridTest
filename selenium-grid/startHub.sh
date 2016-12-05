3be0b2bdf73d435d7fac359b1913d5e085a77675
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-05 14:13:50 +0100

    reference fix to selenium standalone

--- selenium-grid/startHub.sh
+++ selenium-grid/startHub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar selenium-standalone/selenium-server-standalone-3.0.1.jar -role hub -hubConfig configuration/seleniumHubConfiguration.json
+java -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role hub -hubConfig configuration/seleniumHubConfiguration.json
--- selenium-grid/startNodeWindowsEdge.sh
+++ selenium-grid/startNodeWindowsEdge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dwebdriver.edge.driver=drivers/MicrosoftWebDriver.exe -jar selenium-grid/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeEdgeConfiguration.json
+java -Dwebdriver.edge.driver=drivers/MicrosoftWebDriver.exe -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeEdgeConfiguration.json
