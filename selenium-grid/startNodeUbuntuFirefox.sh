f935adb46d8a68f89808574c269f52985cefed2d
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-06 11:36:28 +0100

    config firefox ubuntu working

--- selenium-grid/startNodeUbuntuFirefox.sh
+++ selenium-grid/startNodeUbuntuFirefox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -Dwebdriver.firefox.marionette=drivers/geckodriver -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeFirefoxConfiguration.json
+java -Dwebdriver.gecko.driver=drivers/geckodriver -jar ./selenium-standalone/selenium-server-standalone-3.0.1.jar -role node -nodeConfig configuration/seleniumNodeFirefoxConfiguration.json
 
