d7cbca6febeb5ca2e3f3782aff3aa8f5a176b780
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-07 16:02:09 +0100

    compare creenshots webdrivercss

--- tests/visual-test.js
+++ tests/visual-test.js
@@ -3,6 +3,8 @@ var chai = require('chai')
     , expect = chai.expect
     , should = chai.should();
 var webdrivercss = require('webdrivercss');
+var fs = require("fs");
+var gm = require('gm');
 
 var HomepageMindBlue = require("./page-objects/homepage-mind-blue");
 var homepage;
@@ -12,34 +14,45 @@ describe('title test', function () {
     //hook run before tests
     before(function () {
         homepage = new HomepageMindBlue();
-        return webdrivercss.init(browser).then(function () {
-            return homepage.goToPage();
-        });
+        webdrivercss.init(browser);
+        return homepage.goToPage();
+
     });
 
     it("visual test", function () {
 
-       return webdrivercss.init(browser, {
-            screenshotRoot: 'screenshots',
-            failedComparisonRoot: 'diffs',
+        return webdrivercss.init(browser, {
+            screenshotRoot: 'tests/screenshots/baseline',
+            failedComparisonRoot: 'tests/screenshots/diffs',
             misMatchTolerance: 0.05,
-            screenWidth: [320, 480, 640, 1024]
+            screenWidth: [1024]
         });
     });
 
+    it('should exist an baseline image in the screenshots folder', function (done) {
+        fs.exists('screenshots/baseline/homepage.header.baseline.png', function (exists) {
+            exists.should.equal(true);
+            done();
+        });
+    });
 
     it("visual test", function () {
-        return browser.webdrivercss('startpage', [
+
+        return browser.webdrivercss('homepage',
             {
                 name: 'logo',
-                elem: homepage.logoImageDesktop
-            }, {
-                name: 'header',
-                elem: '.header'
-            }
-        ], function (err, res) {
-           return assert.ok(res.header[0].isWithinMisMatchTolerance);
+                elem: homepage.logoImageDesktop,
+                screenWidth: [1024]
+
+            }).then(function (res) {
+                console.log("res");
+            return gm.compare('tests/screenshots/baseline/homepage.logo.1024px.baseline.png', 'tests/screenshots/baseline/homepage.logo.1024px.baseline.png', function (err, isEqual, equality, raw) {
+                console.log("equality" + " " + equality);
+                return equality.should.be.within(0, 0.0001);
+
+            });
         })
+
 
     });
 });
