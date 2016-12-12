71cddf24d6e3246fcbe350694ba0757bb9d022f3
MMaartenvanWilligen <maarten@connectholland.nl>
2016-12-12 16:44:10 +0100

    add visual test. test output resemble comparison

--- tests/visual-test.js
+++ tests/visual-test.js
@@ -1,100 +1,68 @@
 var chai = require('chai')
     , expect = chai.expect
     , should = chai.should();
+chai.use(require('chai-fs'));
 var webdrivercss = require('webdrivercss');
-var gm = require('gm');
 var resemble = require("node-resemble-js");
-var Promise = require("bluebird");
-var fs = require('fs');
-
-
+var VisualRegression = require("./resemble/visualRegression");
 var HomepageMindBlue = require("./page-objects/homepage-mind-blue");
-var homepage;
+var config = require('../config');
 
 describe('screenshot compare', function () {
 
-    var resultCompare = {};
+    var resultComparison = {};
+    var homepage;
+    var visualRegression;
 
     //hook run before tests
     before(function () {
         homepage = new HomepageMindBlue();
-        webdrivercss.init(browser);
+        visualRegression = new VisualRegression();
         return homepage.goToPage();
     });
 
-    describe('compare data should not be empty', function () {
+    describe('Take screenshot', function () {
 
-        it("should initzialize webdrivercss", function () {
-            return webdrivercss.init(browser, {
-                screenshotRoot: 'tests/screenshots/baseline',
-                failedComparisonRoot: 'tests/screenshots/diffs',
-                screenWidth: [1024]
-            });
+        it("should save a screenshot of the browser view", function () {
+            return browser.saveScreenshot(config.screenshots.baselineImages + 'browserView.png');
         });
 
-        it('should take screenshots with webdrivercss', function () {
-            return browser
-                .webdrivercss('comparisonTest', {
-                    name: 'logo',
-                    elem: homepage.logoImageDesktop
-                });
+
+        it("should save a screenshot of the browser view as regression", function () {
+            return browser.setValue("input[name='q']", "change screenshot").then(function () {
+                return browser.saveScreenshot(config.screenshots.regressionImages + 'browserViewRegression.png');
+            })
         });
 
     });
 
-    describe('comapare baseline screenshot with regression screenshot', function () {
+    describe('Compare baseline image to regression image:', function () {
 
         before(function () {
-
-            resemble.outputSettings({
-                errorColor: {
-                    red: 255,
-                    green: 255,
-                    blue: 0
-                },
-                errorType: 'movementDifferenceIntensity',
-                transparency: 0.3
+            return visualRegression.CompareImages("browserView.png", "browserViewRegression.png").then(function (data) {
+                resultComparison = data;
             });
-
-            return getDataOfComparisonScreenshots();
         });
 
         it("expect data of comparison not to be empty", function () {
-            expect(resultCompare).not.to.be.empty;
+            return expect(resultComparison).not.to.be.empty;
         });
 
-        it('expect screenshots to be same dimension ', function () {
-            expect(resultCompare.isSameDimensions).to.be.true
+        it("expect screenshots to be same dimension ", function () {
+            return expect(resultComparison.isSameDimensions).to.be.true
         });
 
-        function getDataOfComparisonScreenshots() {
-            return new Promise(function (resolve, reject) {
-                resemble('tests/screenshots/baseline/' + file)
-                    .compareTo('tests/screenshots/regression' + file2)
-                    .ignoreColors()
-                    .onComplete(function (data) {
-                        console.log("data" + " " + data.misMatchPercentage);
-                        resolve(data);
-                    });
-            }).then(function (data) {
-                // misMatchPercentage : 100, // %
-                // isSameDimensions: true, // or false
-                // dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
-                // getImageDataUrl: function(){}
-                // console.log("data in then" + " " + data.misMatchPercentage);
-                resultCompare = data;
-                return getDiffImage(data);
-            })
-        }
+        it("expect misMatchPercentage not to be above '0' ", function () {
+            expect(resultComparison.misMatchPercentage).not.to.be.above(0);
+        });
 
-        function getDiffImage(data) {
-            return new Promise(function (resolve, reject) {
-                resolve(data.getDiffImage().pack().pipe(fs.createWriteStream('tests/screenshots/diff/' + diff)));
-            }).then(function (res) {
-                console.log(res);
+        it("should make 'browserViewDiff.png' in " + config.screenshots.diffImages, function () {
+
+            return visualRegression.makeDiffImage(resultComparison).then(function () {
+                expect(config.screenshots.diffImages).to.be.a.path('');
+                //expect(config.screenshots.diffImages).to.not.be.a.path('path does not exist');
             });
-        }
-
+        });
 
     });
 
